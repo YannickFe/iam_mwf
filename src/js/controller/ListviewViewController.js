51df26159995e606652c702573b3b18a4f8d67d6
Yannick Fenz'l <yannick.fenzl@icloud.com>
2025-06-04 13:29:51 +0200

    Remove unused methods in `ListviewViewController` and update list item data binding in template - now using Ractive.js.

--- src/js/controller/ListviewViewController.js
+++ src/js/controller/ListviewViewController.js
@@ -44,51 +44,12 @@ export default class ListviewViewController extends mwf.ViewController {
         this.crudops = GenericCRUDImplLocal.newInstance("MediaItem");
     }
 
-    /*
-     * for views that initiate transitions to other views
-     * NOTE: return false if the view shall not be returned to, e.g. because we immediately want to display its previous view. Otherwise, do not return anything.
-     */
-    async onReturnFromNextView(nextviewid, returnValue, returnStatus) {
-        // TODO: check from which view, and possibly with which status, we are returning, and handle returnValue accordingly
-    }
-
-    /*
-     * for views with listviews: bind a list item to an item view
-     * TODO: delete if no listview is used or if databinding uses ractive templates
-     */
-    bindListItemView(listviewid, itemview, itemobj) {
-        // TODO: implement how attributes of itemobj shall be displayed in itemview
-        itemview.root.getElementsByTagName("img")[0].src = itemobj.src;
-        itemview.root.getElementsByTagName("h2")[0].textContent = itemobj.title + itemobj._id;
-        itemview.root.getElementsByTagName("h3")[0].textContent = itemobj.added;
-    }
 
     /*
      * for views with listviews: react to the selection of a listitem
-     * TODO: delete if no listview is used or if item selection is specified by targetview/targetaction
      */
     onListItemSelected(itemobj, listviewid) {
         // TODO: implement how selection of itemobj shall be handled
         alert("Element " + itemobj.title + itemobj._id + " wurde ausgewählt!");
     }
-
-    /*
-     * for views with listviews: react to the selection of a listitem menu option
-     * TODO: delete if no listview is used or if item selection is specified by targetview/targetaction
-     */
-    onListItemMenuItemSelected(menuitemview, itemobj, listview) {
-        // TODO: implement how selection of the option menuitemview for itemobj shall be handled
-    }
-
-    /*
-     * for views with dialogs
-     * TODO: delete if no dialogs are used or if generic controller for dialogs is employed
-     */
-    bindDialog(dialogid, dialogview, dialogdataobj) {
-        // call the supertype function
-        super.bindDialog(dialogid, dialogview, dialogdataobj);
-
-        // TODO: implement action bindings for dialog, accessing dialog.root
-    }
-
 }
