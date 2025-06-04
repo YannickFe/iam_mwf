aa9f7ae756a2bd546b8e9c6fd6ea35df3261afe1
Yannick Fenz'l <yannick.fenzl@icloud.com>
2025-06-04 13:18:26 +0200

    include item ID in displayed titles and selection alerts

--- src/js/controller/ListviewViewController.js
+++ src/js/controller/ListviewViewController.js
@@ -59,7 +59,7 @@ export default class ListviewViewController extends mwf.ViewController {
     bindListItemView(listviewid, itemview, itemobj) {
         // TODO: implement how attributes of itemobj shall be displayed in itemview
         itemview.root.getElementsByTagName("img")[0].src = itemobj.src;
-        itemview.root.getElementsByTagName("h2")[0].textContent = itemobj.title;
+        itemview.root.getElementsByTagName("h2")[0].textContent = itemobj.title + itemobj._id;
         itemview.root.getElementsByTagName("h3")[0].textContent = itemobj.added;
     }
 
@@ -69,7 +69,7 @@ export default class ListviewViewController extends mwf.ViewController {
      */
     onListItemSelected(itemobj, listviewid) {
         // TODO: implement how selection of itemobj shall be handled
-        alert("Element " + itemobj.title + " wurde ausgewählt!");
+        alert("Element " + itemobj.title + itemobj._id + " wurde ausgewählt!");
     }
 
     /*
