f831690da2a2aabf065cfc7ba99215011e670b0c
Yannick Fenz'l <yannick.fenzl@icloud.com>
2025-06-25 17:09:36 +0200

    Added base MapsViewController & SidemenuViewController as well as update app.html for map view and menu integration

--- src/js/Main.js
+++ src/js/Main.js
@@ -13,6 +13,7 @@ import * as entities from './model/MyEntities.js';
 /* application libraries: view controllers */
 import ListviewViewController from './controller/ListviewViewController';
 import ReadviewViewController from './controller/ReadviewViewController';
+import MapsViewController from './controller/MapsViewController';
 // TODO-REPEATED: import any further view controllers here
 
 // we export the framework modules required by the application and the application modules required by the framework
@@ -23,6 +24,8 @@ mwf.app.components = {
     MyApplication,
     ListviewViewController,
     ReadviewViewController,
+    MapsViewController,
+    SidemenuViewController: mwf.SidemenuViewController,
     // TODO-REPEATED: export any further view controllers here
 };
 
